add01d704722521d0956d8e7e7d43f84d3dffb20
Quyne1204 <nvq12042003@gmail.com>
2024-04-18 00:28:54 +0700

    update

--- app.js
+++ app.js
@@ -14,7 +14,15 @@ const admin = require("./route/admin");
 const app = express();
 dotenv.config();
 
-app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://store-client-b886c.web.app');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
+// app.use(cors());
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
