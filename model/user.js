d8b82d7d626cc1e042e7684cf3dd47b08602d032
Quyne1204 <nvq12042003@gmail.com>
2024-04-18 08:30:39 +0700

    update

--- model/user.js
+++ model/user.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: {
-        type: String,
-        required: false,
-        unique: true
-    },
     fullName: {
         type: String,
         required: false
