18121c315106454da8df95838735666a2fafd6ed
Quyne1204 <nvq12042003@gmail.com>
2024-04-18 08:03:47 +0700

    update

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const cors = require('cors');
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 
 const mongoose = require('mongoose');
@@ -16,12 +15,12 @@ dotenv.config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 app.use(cors());
 app.use('/auth', auth);
 app.use('/product', product);
